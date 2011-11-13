c6c5ae6ea15888898956534dd6d1111c9c539950
Yang Fei <cdredfox@gmail.com>
2011-11-13 08:43:56 +0800

    redesign loginUI,add a feture:modify passwd

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -6,11 +6,17 @@ import play.mvc.With;
 @With(Secure.class)
 public class Users extends Controller {
 	
-	public static boolean passwd(String passwd){
+	public static String passwd(String passwd,String old_passwd){
 		String username=session.get("username");
 		User user=User.find("userName=?", username).first();
+		if(user==null){
+			return "密码修改失败,登录已过期，请重新登录!";
+		}
+		if(!user.passWord.equals(old_passwd)){
+			return "密码修改失败,您的当前密码输入错误，请重新输入后再提交!";
+		}
 		user.passWord=passwd;
 		user.save();
-		return true;
+		return "密码修改成功，请下次登录使用新密码进行登录!";
 	}
 }
